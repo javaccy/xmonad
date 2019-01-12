3f7638c6e8c9a53bc53e3b19f74d34e0f5293b5d
javaccy <javaccy@gmail.com>
2019-01-12 00:21:52 +0000

    Update xmonad.super.sh

--- xmonad.super.sh
+++ xmonad.super.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-cp ~.xmonad/xmonad.super.hs ~.xmonad/xmonad.hs && xmonad --recompile && --restart
+cp ~.xmonad/xmonad.super.hs ~.xmonad/xmonad.hs && xmonad --recompile && xmonad --restart
